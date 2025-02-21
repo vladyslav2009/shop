2a18317a5ec19dfd625c735a330adfef03b9c227
vladyslav2009 <kizimavlad52@gmail.com>
2025-02-22 00:15:35 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Привет, мир!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h2>Это простое веб-приложение на Flask</h2>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+python app.py
